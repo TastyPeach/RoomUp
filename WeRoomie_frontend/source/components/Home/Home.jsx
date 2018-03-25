3f4726e6ab58df49691af1d1cf3c18b811b7922d
Licheng Luo <ll6@illinois.edu>
2018-03-25 13:58:12 -0500

    Login button added

--- WeRoomie_frontend/source/components/Home/Home.jsx
+++ WeRoomie_frontend/source/components/Home/Home.jsx
@@ -6,8 +6,33 @@ import styles from './Home.scss'
 import SubMenu from '../SubMenu.jsx'
 
 export default class Home extends Component {
-
+	constructor()
+	{
+		super();
+	    this.state={login:false};
+		this.loginOnClick=this.loginOnClick.bind(this);
+	}
+	loginOnClick()
+	{
+		this.setState({login:!this.state.login});
+	}
     render() {
+		if(this.state.login==false){
+			return(
+			<div>
+			<div className="userMenu" >
+				<Button onClick={this.loginOnClick}>Login</Button>
+		   	</div>
+            <div className="Home">
+                <h1>WeRoomie</h1>
+                <div className="child">
+                {this.props.children}
+            </div>
+            </div>
+			</div>
+        	)
+		}
+		else{
         return(
 			<div>
 			<div className="userMenu" >
@@ -21,6 +46,7 @@ export default class Home extends Component {
             </div>
 			</div>
         )
-    }
+    	}
+	}
 
 }
