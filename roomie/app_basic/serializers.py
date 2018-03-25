0f6460b8db33cce122d6c67650eaca997da5e10e
Ruilin Zhang <rzhang74@illinois.edu>
2018-03-24 23:20:55 -0500

    serial small change

--- roomie/app_basic/serializers.py
+++ roomie/app_basic/serializers.py
@@ -1,20 +1,20 @@
 from rest_framework import serializers
 
-from app_basic.models import *
+from app_basic.models import *             
 from django.contrib.auth.models import User
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('username', 'first_name', 'last_name', 'email')
+        fields = ('id', 'username', 'first_name', 'last_name', 'email')
 
 class AdvancedUserSerializer(serializers.ModelSerializer):
 	uid = UserSerializer(serializers.ModelSerializer)
-    class Meta:
-        model = AdvancedUser
-        fields = ('gid', 'gender', 'age', 'ehtinicity', 'quiteness', 'sanitary', \
-                  'timetobed', 'pet', 'major', 'hobbies', 'language', 'graduationyear', \
-                  'note')
+	class Meta:
+	    model = AdvancedUser
+	    fields = ('uid', 'gid', 'gender', 'age', 'ethinicity', 'quiteness', 'sanitary', \
+	              'timetobed', 'pet', 'major', 'hobbies', 'language', 'graduationyear', \
+	              'note')
 
 class ApartmentSerializer(serializers.ModelSerializer):
     class Meta:
@@ -22,9 +22,10 @@ class ApartmentSerializer(serializers.ModelSerializer):
         fields = ('aid', 'name', 'capacity', 'price', 'address', 'floorplan', 'occupied')
 
 class GroupSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Group
-        fields = ('gid', 'group_name', 'aid', 'peopleleft', 'admin_uid', 'active')
+	admin_uid = AdvancedUserSerializer(serializers.ModelSerializer)
+	class Meta:
+		model = Group
+		fields = ('gid', 'group_name', 'aid', 'peopleleft', 'admin_uid', 'active')
 
 class PotentialMatch(serializers.ModelSerializer):
     pass
