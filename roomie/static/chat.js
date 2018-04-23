53e0382c7d2cf25f8fb0f870b60f1053f3fbe40e
rzhang74 <rzhang74@illinois.edu>
2018-04-23 03:54:12 -0500

    chat room frontend; scrolling animation

--- roomie/static/chat.js
+++ roomie/static/chat.js
@@ -5,29 +5,39 @@ $(function() {
     
     chatsock.onmessage = function(message) {
         var data = JSON.parse(message.data);
-        var chat = $("#chat")
-        var ele = $('<tr></tr>')
-
+        var chat = $("#chatcontent")
+        var ele = $('<div style="margin-bottom: 15px; text-align:right;"></div>')
+        
         ele.append(
-            $("<td></td>").text(data.timestamp)
+            $("<div></div>").text(data.timestamp).css({"float":"right", "margin-left":"1rem", "font-size":"12px", "font-weight":"200"})
         )
+       
         ele.append(
-            $("<td></td>").text(data.handle)
+            $("<div></div>").text(data.handle).css({"font-weight":"bolder","position":"relative","margin-bottom":"1rem","top": "-3.5px"})
         )
+        
         ele.append(
-            $("<td></td>").text(data.message)
+            $("<div></div>").text(data.message).css({"font-size": "1.11em"}) //.css({"font-size: 1.11em;"})
         )
         
         chat.append(ele)
+        
+        $(document).ready(function(){
+            $(this).scrollTop($(window).height());
+        });
     };
 
     $("#chatform").on("submit", function(event) {
         var message = {
-            handle: $('#handle').text(),
+            handle: $('#handle').text().replace(/\s/g, ''),
             message: $('#message').val(),
         }
         chatsock.send(JSON.stringify(message));
         $("#message").val('').focus();
+        
+        $(document).ready(function(){
+            $(this).scrollTop($(window).height());
+        });
         return false;
     });
 });
